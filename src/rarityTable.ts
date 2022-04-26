17477431a333064fc1c9b68d8c8a1985fed7a354
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2022-04-26 19:16:47 +0200

    random trait combos

--- src/rarityTable.ts
+++ src/rarityTable.ts
@@ -9,6 +9,15 @@ export class RarityTable<T extends string = string> {
         if (this.traits.has(trait)) throw new Error(`Trait ${trait} is already declared in the rarity table.`);
         this.traits.set(trait, new Trait(options));
     }
+    public randomTokenTraits(order: Array<T>): TokenTraits<T> {
+        const traits: TokenTraits<T> = new TokenTraits(order);
+        for (const trait of this.TRAIT_VALUES) {
+            const t = this.traits.get(trait);
+            if (!t) throw new Error(`Missing trait ${trait}`);
+            traits.set(trait, t.randomOption().name);
+        }
+        return traits;
+    }
 }
 export class Trait<O extends string = string> {
     protected readonly options: Map<Option<O>, number> = new Map();
@@ -18,6 +27,35 @@ export class Trait<O extends string = string> {
             this.options.set(option, option.value / sum);
         }
     }
+    public randomOption(random: () => number = Math.random): Option<O> {
+        let rnd = random();
+        for (const option of this.options.keys()) {
+            if (rnd < option.value) return option;
+            rnd -= option.value;
+        }
+        throw new Error("This should never happen.");
+    }
+}
+export class TokenTraits<T extends string = string> extends Map<T, string> {
+    public readonly ALL_TRAITS: Array<T>;
+    constructor(allTraits: Array<T>) {
+        super();
+        this.ALL_TRAITS = allTraits;
+    }
+    toLayerArray(): Array<Layer> {
+        const rv: Array<Layer> = [];
+        for (const trait of this.ALL_TRAITS) {
+            const value = this.get(trait);
+            if (!value) continue;
+            rv.push({name: trait, value});
+        }
+        return rv;
+    }
+
+}
+export interface Layer {
+    readonly name: string;
+    readonly value: string;
 }
 export interface Option<T extends string = string> {
     name: T;
